fe6518bfe46317a76a772ab2a7bcea65128fe523
serfer89 <sergpavlov89@gmail.com>
2018-11-25 01:41:54 +0200

    add schedual

--- forecast.js
+++ forecast.js
@@ -1,24 +1,40 @@
-var cur_datetime = new Date();
+var CronJob = require("cron").CronJob;
+var getJSON = require("get-json");
+
 function Unix_timestamp(t) {
   var dt = new Date(t * 1000);
   return dt.getDate();
 }
-var getJSON = require("get-json");
-getJSON(
-  "http://api.openweathermap.org/data/2.5/forecast/daily?q=Kyiv&appid=d01946c71f847270df726ba46cc786b6&mode=json&units=metric&cnt=2"
-)
-  .then(function(response) {
-  for (var i = 0; i < response.list.length; i++) {
-    if (cur_datetime.getDate() == Unix_timestamp(response.list[i].dt)) {
-      if (response.list[i].temp.morn < 0) {
-        getJSON("http://192.168.0.77:1880/floor").catch(function(error) {
-          console.log(error);
-        });
+
+function getForecast() {
+  var cur_datetime = new Date();
+  getJSON(
+    "http://api.openweathermap.org/data/2.5/forecast/daily?q=Kyiv&appid=d01946c71f847270df726ba46cc786b6&mode=json&units=metric&cnt=2"
+  )
+    .then(function(response) {
+      for (var i = 0; i < response.list.length; i++) {
+        if (cur_datetime.getDate() == Unix_timestamp(response.list[i].dt)) {
+          if (response.list[i].temp.morn < 0) {
+            getJSON("http://192.168.0.77:1880/floor").catch(function(error) {
+              console.log(error);
+            });
+          }
+        }
       }
-    }
-  }
-})
-  .catch(function(error) {
-  console.log(error);
-});
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+new CronJob(
+  "* * 6 * * *",
+  function() {
+    getForecast();
+    console.log("go!");
+  },
+  null,
+  true,
+  "America/Los_Angeles"
+);
 
